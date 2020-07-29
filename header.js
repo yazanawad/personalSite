dce3dfb838d184eb0568aa2fe5acf4515bdc2d3c
HP <awadyazan25@gmail.com>
2020-07-29 15:37:49 -0400

    First Official Site

---
+++ header.js
@@ -0,0 +1,19 @@
+function openNav(){
+    document.getElementById('overlayNav').style.width = '100%'
+}
+function closeNav(){
+    document.getElementById('overlayNav').style.width = '0%';
+}
+var btnOpen = false;
+function myFunction(x) {
+    x.classList.toggle("change");
+    if(btnOpen == false){
+        openNav();
+        btnOpen = true
+    }
+    else{
+        closeNav();
+        btnOpen= false;
+    }
+console.log(btnOpen);
+}
